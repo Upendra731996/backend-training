75d9858073b7028dbebc551befe4ad01aafac892
Upendra731996 <upendragupta731996@gmail.com>
2022-08-09 02:28:18 +0530

    2nd half assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,10 +10,112 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
+
+
+
+
 router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
 })
+router.get('/movie',function(req ,res){
+
+    let movie=['rajababu','koyla,nayak','krish', 'zurm']
+    res.send(movie)
+})
+
+
+
+
+router.get('/movie/:indexNuber',function(req,res){
+    
+
+
+    let movie=['rajababu','koyla','nayak','krish', 'zurm']
+
+
+let requestParams=req.params.indexNuber
+
+if(requestParams<movie.length){
+
+console.log("movie name is ",movie[requestParams])
+}
+else{
+console.log("invalid input hai")
+    res.send("invalid input")
+    
+}
+res.send("movie is available go and watch")
+
+})
+
+router.get('/films',function(req,res){
+    let films=
+
+    [ {
+    "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+res.send(films)
+
+console.log(films)
+
+})
+
+router.get('/films/:filmId', function(req,res){
+
+    let id=req.params.filmId
+
+
+
+
+    let films=
+
+    [ {
+    "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+       for(let i=0;i <films.length;i++){
+
+        if(id==i+1){
+
+            console.log(films[i])
+        
+        }
+        
+
+        
+     }
+       
+    }
+
+
+)
+
+
+
+
+
 
 router.get('/student-details/:name', function(req, res){
     /*
@@ -28,7 +130,7 @@ router.get('/student-details/:name', function(req, res){
 
     // JSON strigify function helps to print an entire object
     // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
+    console.log("This is the request "+JSON.stringify (requestParams))
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
     
