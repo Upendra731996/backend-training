c87ea5da4f22e888289c886041e829f9281e707d
Upendra731996 <upendragupta731996@gmail.com>
2022-08-11 00:03:53 +0530

    postplayer and query-param assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -103,5 +103,64 @@ router.get("/films/:filmId", function(req, res){
        res.send("The film id doesn't match any movie")
 })
 
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+                          ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+
+   router.post('/players', function (req, res) {
+    
+    let postplr= req.body
+    let newPlayersName = postplr.name
+    let isNameRepeated = false
+
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    
+    if (isNameRepeated) {
+        
+        res.send("This player was already added")
+    } else {
+        
+        players.push(postplr)
+        res.send(players)
+    }
+});
+
+ 
 module.exports = router;
 // adding this comment for no reason
