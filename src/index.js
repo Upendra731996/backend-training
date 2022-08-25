cfc2d08f2682e72d799797260e0236d456a737e2
Upendra731996 <upendragupta731996@gmail.com>
2022-08-25 15:04:03 +0530

    index in solution

--- src/index.js
+++ src/index.js
@@ -2,24 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const moment = require('moment');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://upendra:wvUNUF1FjJ02PCPH@cluster0.b8yrh4n.mongodb.net/upendra123?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
+const time =moment()
+app.use ('/account',
     function (req, res, next) {
         console.log ("inside GLOBAL MW");
+        
+        
+
         next();
+       
   }
   );
+
+app.use('/account', function(req, res, next) {
+    let timeStamp= moment().format("DD-MM-YYYY,HH:mm:ss")//new Date( )
+    let ipaddress=req.ip;
+    let url=req.originalUrl
+    console.log(timeStamp,ipaddress,url )
+      next();
+    
+  });
+
+
+
+
 
 app.use('/', route);
 
